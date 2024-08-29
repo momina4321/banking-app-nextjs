cc5169090093a51decd6bc97dcfb8c02773e3a74
momina-bs <momina.byteshifted@gmail.com>
2024-08-30 01:25:02 +0500

    sidefooter changes

--- app/components/sidefooter.tsx
+++ app/components/sidefooter.tsx
@@ -15,12 +15,14 @@ export default function SideFooter() {
 
     return (
         <div className="flex px-5 items-center justify-between py-6 border-t-2 border-[#EAECF0] mt-auto">
+            <div className="flex gap-2 justify-between">
             <Image src="/jsmastery.svg" width={40} height={40} alt="js" />
             <div className="flex flex-col gap-1">
-                <div className="font-semibold">{user.fname} {user.lname}</div>
-                <div className="lg:block hidden text-[#475467]">{user.email}</div>
+                <div className=" flex font-semibold">{user.fname} {user.lname}</div>
+                <div className="lg:flex  hidden text-[#475467]">{user.email}</div>
             </div>
-            <div className="flex flex-col">
+            </div>
+            <div className="flex flex-col  ">
 
                 <button onClick={() => userLogout()} >
                     <Image src="/log-out-01.svg" width={20} height={20} alt="logout" />
